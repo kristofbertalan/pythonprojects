ac4af96627c7f1ff6d533cd223c86bddcaabf929
kristofbertalan <christian19980621@gmail.com>
2021-12-10 18:14:16 +0100

    Removed print statements

--- Advent of Code/Day 1/main.py
+++ Advent of Code/Day 1/main.py
@@ -2,10 +2,6 @@
 def exerciseone(input):
     count = 0
     for x, y in zip(input, input[1:]):
-        if count > 1450:
-            print(x)
-            print(y)
-            print(count)
         if x < y:
             count += 1
     return count
